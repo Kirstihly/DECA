4176a896eb8a4f112a165f528df947dcf1394e5f
Leying Hu <leying.hu@innopeaktech.com>
2023-04-04 16:29:16 -0700

    env change for cuda11

--- install_conda.sh
+++ install_conda.sh
@@ -3,8 +3,11 @@
 export CONDA_ENV_NAME=deca-env
 echo $CONDA_ENV_NAME
 
-conda create -n $CONDA_ENV_NAME python=3.7
+conda create -n $CONDA_ENV_NAME python=3.9
 
 eval "$(conda shell.bash hook)"
 conda activate $CONDA_ENV_NAME
 pip install -r requirements.txt
+pip install ./torch-1.9.1+cu111-cp39-cp39-linux_x86_64.whl
+pip install ./torchvision-0.10.1+cu111-cp39-cp39-linux_x86_64.whl
+pip install git+https://github.com/facebookresearch/pytorch3d.git@stable
